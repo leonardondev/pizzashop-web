9b6045974bf3fc888f10864def02095b7069262c
Leonardo do Nascimento <leonardo3.nascimento@gmail.com>
2024-10-07 23:38:21 -0300

    feat: login com tanstack-query e magic link

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,9 +1,11 @@
+import { useMutation } from '@tanstack/react-query'
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
 import { Link } from 'react-router-dom'
 import { toast } from 'sonner'
 import { z } from 'zod'
 
+import { signIn } from '@/api/sign-in'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
@@ -18,21 +20,26 @@ export function SignIn() {
   const {
     register,
     handleSubmit,
-    reset,
     formState: { isSubmitting },
   } = useForm<SignInForm>()
 
-  async function handleSignIn(data: SignInForm) {
-    await new Promise((resolve) => setTimeout(resolve, 2000))
-    console.log(data)
+  const { mutateAsync: authenticate } = useMutation({
+    mutationFn: signIn,
+  })
 
+  async function handleSignIn(data: SignInForm) {
     if (!data.email) {
       return toast.error('Email inválido')
     }
 
-    toast.success('Enviamos um link de autenticação para o seu email')
+    await authenticate({ email: data.email })
 
-    reset()
+    toast.success('Enviamos um link de autenticação para o seu email.', {
+      action: {
+        label: 'Reenviar',
+        onClick: () => handleSignIn(data),
+      },
+    })
   }
 
   return (
