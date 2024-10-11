a5b9b128d13a2034b453316b3e0395ba06ee20ff
Leonardo do Nascimento <leonardo3.nascimento@gmail.com>
2024-10-10 22:20:11 -0300

    dev: atualização do perfil e do HTTP State (cached)

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -21,12 +21,14 @@ export function AccountMenu() {
   const { data: profile, isLoading: isLoadingProfile } = useQuery({
     queryKey: ['profile'],
     queryFn: getProfile,
+    staleTime: Infinity, // Tempo para informação se tornar obsoleta (aqui não é necessário)
   })
 
   const { data: managedRestaurant, isLoading: isLoadingManagedRestaurant } =
     useQuery({
       queryKey: ['managed-restaurant'],
       queryFn: getManagedRestaurant,
+      staleTime: Infinity, // Tempo para informação se tornar obsoleta (aqui não é necessário)
     })
 
   return (
