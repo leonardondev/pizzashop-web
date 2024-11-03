b68166effed0763f14e581a9cdf10cd0cdd5c2e7
Leonardo do Nascimento <leonardo3.nascimento@gmail.com>
2024-11-02 23:17:21 -0300

    deps: add dependence to deploy

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import './global.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import { App } from './app'
+import { App } from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
