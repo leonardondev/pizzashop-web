9a3445f1febde158f9297ea1c1f791c623e7fdf7
Leonardo do Nascimento <leonardo3.nascimento@gmail.com>
2024-11-08 22:58:31 -0300

    test: usando o testing-library em componentes

--- src/components/order-status.tsx
+++ src/components/order-status.tsx
@@ -26,19 +26,31 @@ export function OrderStatus({ status }: OrderStatusProps) {
   return (
     <div className="flex items-center gap-2">
       {status === 'pending' && (
-        <div className="h-2 w-2 rounded-full bg-slate-400" />
+        <div
+          className="h-2 w-2 rounded-full bg-slate-400"
+          data-testid="badge"
+        />
       )}
       {status === 'canceled' && (
-        <div className="h-2 w-2 rounded-full bg-rose-500" />
+        <div className="h-2 w-2 rounded-full bg-rose-500" data-testid="badge" />
       )}
       {status === 'processing' && (
-        <div className="h-2 w-2 rounded-full bg-amber-500" />
+        <div
+          className="h-2 w-2 rounded-full bg-amber-500"
+          data-testid="badge"
+        />
       )}
       {status === 'delivering' && (
-        <div className="h-2 w-2 rounded-full bg-amber-500" />
+        <div
+          className="h-2 w-2 rounded-full bg-amber-500"
+          data-testid="badge"
+        />
       )}
       {status === 'delivered' && (
-        <div className="h-2 w-2 rounded-full bg-emerald-500" />
+        <div
+          className="h-2 w-2 rounded-full bg-emerald-500"
+          data-testid="badge"
+        />
       )}
 
       <span className="font-medium text-muted-foreground">
